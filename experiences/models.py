fd856d9f3c8db3e3860659956690163875091dce
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-04-18 21:20:52 +0300

    Add Experience model to admin site and allow blank fields
    * In `admin.py`, the Experience model is registered to be displayed on the admin site
    * In `models.py`, the `startDate`, `endDate`, `description

--- experiences/models.py
+++ experiences/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Experience(models.Model):
   companyName = models.CharField(max_length=100)
   jobTitle = models.CharField(max_length=100)
-  startDate = models.DateTimeField()
+  startDate = models.DateTimeField(blank=True)
   endDate = models.DateTimeField(blank=True, null=True)
-  description = models.TextField()
-  slug = models.SlugField(unique=True)
+  description = models.TextField(blank=True)
+  slug = models.SlugField(blank=True, unique=True)
+
+
+  def __str__(self):
+    return self.companyName

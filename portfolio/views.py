74b1ab23ae25fc419be4f470eb0a5f719d38b8c7
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-04-18 14:49:44 +0300

    Add templates directory to settings and update views to use render shortcut
    -In settings.py, add "templates" to DIRS in TEMPLATES.
    -In views.py, update home and about views to use

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-  return HttpResponse("home")
+  # return HttpResponse("home")
+  return render(request, 'home.html')
 
 def about(request):
-  return HttpResponse("about")
+  # return HttpResponse("about")
+  return render(request, 'about.html')

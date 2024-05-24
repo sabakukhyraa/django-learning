fc83665def8276c54e864b27394f09519d7c3ce3
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-05-24 21:09:33 +0300

    trying

--- generalsettings/apps.py
+++ generalsettings/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GeneralsettingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'generalsettings'
+    name = 'generalSettings'
--- generalsettings/urls.py
+++ generalsettings/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import generalSetting
 
 urlpatterns = [
-  path('', views.generalSetting),
+  path('', generalSetting, name='home')
 ]
